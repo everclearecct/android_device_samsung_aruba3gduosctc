0407d17e729f9a181359956d042a27af940af9fe
everclearecct <cyantrigger@gmail.com>
2021-10-30 10:30:31 +0800

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_aruba3gduosctc.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_aruba3gduosctc-user \
+    omni_aruba3gduosctc-userdebug \
+    omni_aruba3gduosctc-eng
---
+++ omni_aruba3gduosctc.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from aruba3gduosctc device
+$(call inherit-product, device/samsung/aruba3gduosctc/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := aruba3gduosctc
+PRODUCT_NAME := omni_aruba3gduosctc
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SCH-I829
+PRODUCT_MANUFACTURER := samsung
